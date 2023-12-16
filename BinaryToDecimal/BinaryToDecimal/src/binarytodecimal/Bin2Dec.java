13cb15066140906d49e7824394ffdee84e6582e0
Nakiri00 <124451536+Nakiri00@users.noreply.github.com>
2023-12-17 00:49:00 +0900

    Bin2Dec.java
    App Ideas from Florin pop

--- BinaryToDecimal/BinaryToDecimal/src/binarytodecimal/Bin2Dec.java
+++ BinaryToDecimal/BinaryToDecimal/src/binarytodecimal/Bin2Dec.java
@@ -1,13 +1,13 @@
 package binarytodecimal;
+import java.util.Arrays;
 import java.util.Scanner;
 //import java.math.*;
 
 
 public class Bin2Dec {
     public static void main(String[] args) {
-        // TODO code application logic here
         Scanner in = new Scanner(System.in);
-        int[] binary = {1,0,1,1,0,1,0,0};
+        // int[] binary = {1,0,1,1,0,1,0,0};
         int digitBinary;
         System.out.println("Dynamic Input");
         System.out.print("Enter the total digit of binary code : ");
@@ -21,25 +21,26 @@ public class Bin2Dec {
                System.out.print(l+1 +" : ");binary2[l] = in.nextInt();
             }
         }
-        int decimal = 0;
+        // int decimal = 0;
         int decimal2 = 0;
         int temp = 0;   
         int i = 0;
-        int j = binary.length - 1;
+        int j = binary2.length - 1;
         while(i <= j){    
             for(int k = 0;k<=j-i;k++){
                 temp = (int) Math.pow(2, k);        
             }
-            decimal += binary[i] * temp;
+            // decimal += binary[i] * temp;
             decimal2 += binary2[i] *temp;
             i++;
         }
+        System.out.println("Binary Code  : " +Arrays.toString(binary2));
         System.out.println("Decimal Code : " + decimal2);
         System.out.println("");
-        System.out.println("Static Input");
-        System.out.println("Binary Code  : "+binary[0]+binary[1]+binary[2]+binary[3]+binary[4]+binary[5]+binary[6]+binary[7]);
-        System.out.println("Decimal Code : " + decimal);
-        System.out.println("");
+        // System.out.println("Static Input");
+        // System.out.println("Binary Code  : "+Arrays.toString(binary));
+        // System.out.println("Decimal Code : " + decimal);
+        // System.out.println("");
     }
     
 }
