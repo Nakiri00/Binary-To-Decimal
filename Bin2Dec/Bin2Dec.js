cfc11c75fbddded3a82901deae0b20e3dbb5e932
Nakiri00 <124451536+Nakiri00@users.noreply.github.com>
2023-12-17 00:50:56 +0900

    Bin2Dec.js

--- Bin2Dec/Bin2Dec.js
+++ Bin2Dec/Bin2Dec.js
@@ -1,10 +1,10 @@
-let binary = [1,0,1,1,0,1,0,0];
+// let binary = [1,0,1,1,0,1,0,0];
 let binary2 = new Array();
 let decimal=0;
 let decimal2 = 0; 
 let temp=0;
 
-let DigitBinary = prompt("Enter the digit of binary code : ");
+let DigitBinary = prompt("Enter the total digit of binary code : ");
 for(let l = 0; l<=DigitBinary - 1 ;l++){
     binary2[l] = prompt(l+1 +" : ");
     while(binary2[l] > 1 || binary2[l] < 0 || binary2[l].length < 1 || binary2[l].length > 1){
@@ -14,17 +14,17 @@ for(let l = 0; l<=DigitBinary - 1 ;l++){
 }
 
 let i = 0;
-let j = (binary.length)-1;
+let j = (binary2.length)-1;
 while(i <= j){
     for(let k = 0; k <= j-i; k++){
         temp = 2**k;
     }
-    decimal += binary[i] * temp;
+    // decimal += binary[i] * temp;
     decimal2 += binary2[i] * temp;
     i++;
 }
-console.log("Static Input");
-console.log("Decimal Code of ["+ binary.toString()+ "] : " +decimal);
-console.log("");
+// console.log("Static Input");
+// console.log("Decimal Code of ["+ binary.toString()+ "] : " +decimal);
+// console.log("");
 console.log("Dynamic Input");
 console.log("Decimal Code of ["+ binary2.toString()+ "] : " +decimal2);
