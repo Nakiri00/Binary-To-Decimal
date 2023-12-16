6a300c5479734a43e03a12bcfef6e2075fed673b
Nakiri00 <124451536+Nakiri00@users.noreply.github.com>
2023-12-17 00:32:35 +0900

    Bin2Dec.java

--- BinaryToDecimal/BinaryToDecimal/src/binarytodecimal/Bin2Dec.java
+++ BinaryToDecimal/BinaryToDecimal/src/binarytodecimal/Bin2Dec.java
@@ -7,11 +7,14 @@ public class Bin2Dec {
     public static void main(String[] args) {
         // TODO code application logic here
         Scanner in = new Scanner(System.in);
-        int[] binary2 = new int[8];
         int[] binary = {1,0,1,1,0,1,0,0};
+        int digitBinary;
         System.out.println("Dynamic Input");
+        System.out.print("Enter the total digit of binary code : ");
+        digitBinary = in.nextInt();
+        int[] binary2 = new int[digitBinary];
         System.out.println("Enter the Binary code");
-        for(int l = 0; l < 8 ; l++){
+        for(int l = 0; l < digitBinary ; l++){
             System.out.print(l+1 +" : ");binary2[l] = in.nextInt();
             while(binary2[l] > 1 || binary2[l] < 0){
                System.out.println("It has to be a binary code, try again");
