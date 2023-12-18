5ac175ea53d342ed4759bd1272f9febd7a9bec64
Nakiri00 <124451536+Nakiri00@users.noreply.github.com>
2023-12-18 21:59:12 +0900

    Bin2Dec(NoArray).js

--- Bin2Dec/Bin2Dec(NoArray).js
+++ Bin2Dec/Bin2Dec(NoArray).js
@@ -1,16 +1,11 @@
-// let binary = [1,0,1,1,0,1,0,0];
-// let binary2 = new Array();
-// let decimal=0;
-let decimal2 = 0; 
+let decimal = 0; 
 let temp=0;
 
 let binary = prompt('Input the binary code : ');
 if(binary == ''){
     binary =0;
 }
-// let DigitBinary = prompt("Enter the total digit of binary code : ");
 for(let l = 0; l<=binary.length - 1 ;l++){
-//     binary2[l] = prompt(l+1 +" : ");
     while(binary[l] > 1 || isNaN(binary[l])){
         alert("Please input a binary code correctly");
         binary = prompt('Input the binary code : ');
@@ -24,12 +19,8 @@ while(i <= j){
     for(let k = 0; k <= j-i; k++){
         temp = 2**k;
     }
-    // decimal += binary[i] * temp;
-    decimal2 += binary[i] * temp;
+    decimal += binary[i] * temp;
     i++;
 }
-// console.log("Static Input");
-// console.log("Decimal Code of ["+ binary.toString()+ "] : " +decimal);
-// console.log("");
 console.log("Dynamic Input");
-console.log("Decimal Code of "+ binary + " : " +decimal2);
+console.log("Decimal Code of "+ binary + " : " +decimal);
